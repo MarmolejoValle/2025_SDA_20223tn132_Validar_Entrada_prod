e38b175bfcb2cb421b7b8f58a06c5ab9d646a3da
MarmolejoValle <20223tn132@utez.edu.mx>
2025-02-10 10:54:04 -0600

    verificacion

--- script.js
+++ script.js
@@ -3,40 +3,126 @@ const tel = document.getElementById('tel');
 const rfc = document.getElementById('rfc');
 const contact = document.getElementById('contact');
 const mail = document.getElementById('mail');
+const forms = document.getElementById('forms');
+const button = document.getElementById('button');
+button.style.backgroundColor = 'black';
 
 
-rs.addEventListener('change',()=>{
-    const alertL  = document.getElementById('rs-avis');
 
-    if(!rs.value){
-        alertLabel(alertL,"Necesitas ingresar una razon social")
-        return;
-    }
-    alertLabel(alertL,"");
-})
-
-tel.addEventListener('change',()=>{
-    const alertL  = document.getElementById('tel-avis');
-
-    if(!tel.value){
-        alertLabel(alertL,"Necesitas ingresar una telefono")
-        return;
-    }
-    if(!(tel.value.toString().charAt(0) == "+")){
-        alertLabel(alertL,"No tiene el signo +")
-        return;
-    }
-    if(tel.value.toString().length > 13){
-        alertLabel(alertL,"No pueden ser mas de 13 valores")
-        return;
-    }
-    if(tel.value.toString().length < 13){
-        alertLabel(alertL,"Numero de telefono incompleto")
-        return;
-    }
-    alertLabel(alertL,"");
-})
-
-function alertLabel(label,text){
+function alertLabel(label, text) {
     label.innerText = text;
+    label.style.color = 'red'
+}
+
+function verificRS() {
+    const alertL = document.getElementById('rs-avis');
+
+    if (!rs.value) {
+        alertLabel(alertL, "Necesitas ingresar una razon social")
+        return false;
+    }
+    alertLabel(alertL, "");
+    return true;
+
+}
+
+function verificTel() {
+    const alertL = document.getElementById('tel-avis');
+    if (!tel.value || tel.value == "") {
+        alertLabel(alertL, "Necesitas ingresar una telefono")
+        return false;
+    }
+    if (!(tel.value.toString().charAt(0) == "+")) {
+        alertLabel(alertL, "No tiene el signo +")
+        return false;
+    }
+    if (tel.value.toString().length > 13) {
+        alertLabel(alertL, "No pueden ser mas de 13 valores")
+        return false;
+    }
+    if (tel.value.toString().length < 13) {
+        alertLabel(alertL, "Numero de telefono incompleto")
+        return false;
+    }
+    alertLabel(alertL, "");
+    return true;
+
+}
+function verificRFC() {
+    const rfcEmpresaRegex = /^[A-ZÑ&]{3}\d{6}[A-Z0-9]{3}$/;
+
+    const alertL = document.getElementById('rfc-avis');
+
+    if (!rfc.value) {
+        alertLabel(alertL, "Necesitas ingresar un rfc empresarial")
+        return false;
+    }
+    if (!rfcEmpresaRegex.test(rfc.value)) {
+        alertLabel(alertL, "Formato de RFC no valido")
+        return false;
+    }
+    alertLabel(alertL, "");
+    return true;
+
+}
+function verificMail() {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    const alertL = document.getElementById('mail-avis');
+
+    if (!mail.value) {
+        alertLabel(alertL, "Necesitas ingresar un correo electronico")
+        return false;
+    }
+    if(!emailRegex.test(mail.value)){
+        alertLabel(alertL, "Correo electronico invalido")
+        return false;
+    }
+    alertLabel(alertL, "");
+    return true;
+
+}
+
+function verificContact() {
+    const alertL = document.getElementById('contact-avis');
+
+    if (!contact.value) {
+        alertLabel(alertL, "Necesitas ingresar un contacto")
+        return false;
+    }
+    alertLabel(alertL, "");
+    return true;
+
+}
+
+
+forms.addEventListener('submit', (event) => {
+    if (allVerific()) return
+    event.preventDefault();
+})
+
+
+function allVerific() {
+    if (verificRS() &&
+        verificTel() &&
+        verificRFC() &&
+        verificMail() &&
+        verificContact()) {
+        button.style.backgroundColor = '';
+        return true
+    }
+
+    rs.addEventListener('change', () => allVerific())
+
+    tel.addEventListener('change', () => allVerific())
+
+    contact.addEventListener('change', () => allVerific())
+
+    mail.addEventListener('change', () => allVerific())
+
+    rfc.addEventListener('change', () => allVerific())
+
+    button.style.backgroundColor = 'black';
+    return false
+
 }
